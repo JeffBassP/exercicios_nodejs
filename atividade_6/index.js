809e7be64f8afd7c45edc03775dea3d583e9eb0e
JeffBassP <jeferson_jfs@hotmail.com>
2020-06-22 13:55:27 -0300

    Finalizando a atividade 6

--- atividade_6/index.js
+++ atividade_6/index.js
@@ -1,16 +1,25 @@
 let rs = require('readline-sync');
 
 // ## Faça o código ##
-let nome = rs.question('Digite seu nome:');
-let palavra = rs.question('Digite uma palavra ou frase: ')
-palavra = String(palavra);
-let letras = 0
+let nome1 = rs.question('NOME 1 - Digite seu nome: ');
+let palavra1 = rs.question('PALAVRA 1 - Digite uma palavra ou frase: ')
+let nome2 = rs.question('NOME 2 - Digite seu nome: ');
+let palavra2 = rs.question('PALAVRA 2 - Digite uma palavra ou frase: ')
+palavra1 = String(palavra1);
+palavra2 = String(palavra2);
+let letras1 = palavra1.length;
+let letras2 = palavra2.length;
 
-while (letras < palavra.length)
+if (letras1 > letras2)
 {
-    letras++;
+    console.log( nome1+ ' usou '+ letras1+ ' letras na palavra '+ palavra1+'.');
+    console.log( nome2+ ' usou '+ letras2+ ' letras na palavra '+ palavra2+'.');
+    console.log( nome1+ ' venceu!!')
+    
 }
-
-
-
-console.log('A palavra/frase "'+ palavra + '", Tem '+ letras+ ' letras.');
+else 
+{
+    console.log(nome1+ ' usou '+ letras1+ ' letras na palavra '+ palavra1+'.');
+    console.log(nome2+ ' usou '+ letras2+ ' letras na palavra '+ palavra2+'.');
+    console.log(nome2+ ' venceu!!')
+}
